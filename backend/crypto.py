2e7e34ce1c38c5bd4495c4de202dc3b9315380cf
Ishan Teredesai <tereishan@outlook.com>
2026-02-28 22:35:28 +0530

    encryption function and route

---
+++ backend/crypto.py
@@ -0,0 +1,25 @@
+import base64
+import hashlib
+from cryptography.fernet import Fernet
+from qiskit import QuantumCircuit, transpile
+from qiskit_aer import Aer
+
+backend = Aer.get_backend("aer_simulator")
+
+def generate_quantum_key():
+    qc = QuantumCircuit(4, 4)
+    qc.h(range(4))
+    qc.measure(range(4), range(4))
+
+    qc_compiled = transpile(qc, backend)
+    result = backend.run(qc_compiled, shots=1).result()
+    counts = result.get_counts()
+
+    bits = list(counts.keys())[0]
+    return hashlib.sha256(bits.encode()).digest()
+
+
+def encrypt_data(data: str, key: bytes):
+    fernet_key = base64.urlsafe_b64encode(key)
+    f = Fernet(fernet_key)
+    return f.encrypt(data.encode())
--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,13 @@ import os
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, HTTPException
+import time
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
+from db import get_connection
+from crypto import generate_quantum_key, encrypt_data, decrypt_data
 
 load_dotenv()
 
@@ -16,12 +22,44 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/home")
+@app.get("/ping")
 def home():
 
     return {
-        "status": "success",
+        "message": "pong",
     }
+
+class Payload(BaseModel):
+    id: str
+    data: str
+
+@app.post("/encrypt")
+def encrypt(payload: Payload):
+
+    start = time.perf_counter()
+
+    key = generate_quantum_key()
+    encrypted = encrypt_data(payload.data, key)
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "INSERT INTO secure_storage (id, encrypted_data) VALUES (%s, %s)",
+        (payload.id, encrypted)
+    )
+
+    conn.close()
+
+    total_time = (time.perf_counter() - start) * 1000
+
+    return {
+        "status": "stored",
+        "processing_time_ms": round(total_time, 3)
+    }
+
+
+
 
 if __name__ == "__main__":
     import uvicorn
